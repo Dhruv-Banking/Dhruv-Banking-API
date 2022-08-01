2d38bf11a11544be20c34bcae36e8122739f7734
Dhruv-Rayat1 <dhruvrayat50@gmail.com>
2022-08-02 06:54:23 +1200

    COMMENTS

--- routes/Get/specificUser.js
+++ routes/Get/specificUser.js
@@ -1,40 +1,62 @@
-const express = require("express");
-let router = express.Router();
-const { Pool } = require("pg");
-require("dotenv").config({ path: "../../.env" });
-const auth = require("../../middleware/auth/auth");
+// Imports
+const express = require("express"); // Express as an API
+let router = express.Router(); // Router
+const { Pool } = require("pg"); // Query the database
+require("dotenv").config({ path: "../../.env" }); // Dotenv, to read a .env file
+const auth = require("../../middleware/auth/auth"); // Authentication
 
+// Connection string from the dotenv file
 const connectionString = process.env.CONNECTIONSTRING;
 
+// Allowing out app to use json in the request body
+router.use(express.json());
+
+// Creating a connection pool
 const pool = new Pool({
   connectionString,
 });
 
+// This is the endpoint to get a specific user
+// It takes one query parameter:
+// @request.query.username
 router.get("/", auth.authenticateToken, (req, res) => {
+  // This is a variable to check if the user is real
   let realUser = true;
-  const username = req.query.username;
+  const username = req.query.username; // username from the query
 
+  // Checking if user is null, or not provided
   if (username === "" || username === undefined) {
     realUser = false;
   }
 
+  // This is the query to get everything, but the password (for security reasons)
   const query =
     "SELECT uuid, username, firstname, lastname, email, savings, checkings FROM users WHERE username=$1";
   const values = [username];
 
+  // If the user is real, then we execute the program
   if (realUser) {
+    // Querying the databse
     pool.query(query, values, (err, sqlRes) => {
+      // If err, then we throw an error
       if (err) {
         res.status(500).send({ detail: err.stack });
-      } else if (sqlRes.rowCount === 0) {
+      }
+      // If the returned rows is negative, then we know the user does not exist
+      else if (sqlRes.rowCount === 0) {
         res.status(400).send({ detail: "User does not exist" });
-      } else {
+      }
+      // else we send back the data
+      else {
         res.send(sqlRes.rows);
       }
     });
-  } else {
+  }
+  // else we know the api caller did not specify a user
+  else {
     res.status(400).send({ detail: "Please provide user." });
   }
 });
 
+// Exporting the module so we can use it from the main file
 module.exports = router;
