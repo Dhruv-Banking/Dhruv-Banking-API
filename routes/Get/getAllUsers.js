3111078da1eef1f74cf30d1cf810b79945573f17
Dhruv-Rayat1 <dhruvrayat50@gmail.com>
2022-08-02 06:46:54 +1200

    COMMENTS

--- routes/Get/getAllUsers.js
+++ routes/Get/getAllUsers.js
@@ -1,17 +1,23 @@
-const express = require("express");
-let router = express.Router();
-const { Pool } = require("pg");
-require("dotenv").config({ path: "../../.env" });
-const auth = require("../../middleware/auth/auth");
+// Imports
+const express = require("express"); // Express as an API
+let router = express.Router(); // Router
+const { Pool } = require("pg"); // Query the database
+require("dotenv").config({ path: "../../.env" }); // Dotenv, to read a .env file
+const auth = require("../../middleware/auth/auth"); // Authentication
 
+// Connection string from the dotenv file
 const connectionString = process.env.CONNECTIONSTRING;
 
+// Creating a connection pool
 const pool = new Pool({
   connectionString,
 });
-
+// This is the endpoint to delete a user, it takes 0 Parameters
+// instead it takes only a token.
 router.get("/", auth.authenticateToken, (req, res) => {
+  // querying the database for all users
   pool.query('SELECT * FROM "public"."users" LIMIT 100', (err, sqlRes) => {
+    // If err, then throw an error, else we just send back the array
     if (err) {
       res.status(500).send({ detail: err.stack });
     } else {
@@ -20,4 +26,5 @@ router.get("/", auth.authenticateToken, (req, res) => {
   });
 });
 
+// Exporting the module so we can use it from the main file
 module.exports = router;
