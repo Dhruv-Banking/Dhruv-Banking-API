9d0bc95657c5f3d8500ff4b9e4ee4d5ea680dfd7
Dhruv-Rayat1 <dhruvrayat50@gmail.com>
2022-08-07 10:44:44 +1200

    Update so 1 ip address can not make more than 1 account

---
+++ middleware/flag-ip-address/flagIpAddress.js
@@ -0,0 +1,51 @@
+const pool = require("../../database/pool"); // Pooling the connections to one pool
+const roleData = require("../roles/roleData");
+const jwt = require("jsonwebtoken");
+
+function flagIpAddress(req, res, next) {
+  const ip = req.ip;
+  const query = "SELECT * FROM flagged_ips WHERE ip=$1";
+  const values = [ip];
+
+  // check to see if the ip exists.
+  pool.query(query, values, (err, sqlres) => {
+    if (err) {
+      res.staus(500);
+      return res.send({ detail: "Internal Server Error" });
+    } else if (sqlres.rowCount === 0) {
+      // else if they don't exist, then we just add them
+      const addUserQuery = "INSERT INTO flagged_ips (ip) VALUES ($1)";
+      const addUserValues = values;
+      pool.query(addUserQuery, addUserValues, (error, sqlResTwo) => {
+        if (error) {
+          res.staus(500);
+          return res.send({ detail: "Internal Server Error" });
+        } else {
+          next();
+        }
+      });
+    } else {
+      // IF they are god, then we let them make the the accunt
+      var authHeader = req.headers["authorization"];
+      const token = authHeader && authHeader.split(" ")[1]; // Spliting becase it goes: "Bearer [space] TOKEN"
+      let decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+      let role = decoded.role;
+
+      if (role === roleData.god || role === roleData.admin) {
+        next();
+      } else {
+        res.status(400);
+        return res.send({
+          detail: "You already exist, please use your previous account",
+        });
+      }
+    }
+  });
+
+  // If not, then continue
+}
+
+module.exports = {
+  flagIpAddress,
+};
