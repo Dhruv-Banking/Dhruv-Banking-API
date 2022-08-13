d69ea78d9287182195d080660d6abbabe56e8fdc
Dhruv-Rayat1 <dhruvrayat50@gmail.com>
2022-08-14 11:21:15 +1200

    Refactor to make deleteUser.js to include OOP, and a smaller scope

--- routes/Delete/deleteUser.js
+++ routes/Delete/deleteUser.js
@@ -4,23 +4,24 @@ let router = express.Router(); // Router
 const pool = require("../../database/pool"); // Pooling the connections to one pool
 const auth = require("../../middleware/auth/auth"); // Authentication
 const bcrypt = require("bcrypt"); // Encryption
-require("dotenv").config({path: "../../.env"}); // Dotenv, to read a .env file
 
 // Allowing our app to use json in the request body
 router.use(express.json());
 
+// Delete User class
+class deleteUser {
+    constructor(username, password) {
+        this.username = username;
+        this.password = password;
+    }
+}
+
 // This is the endpoint to delete a user, it takes 2 Parameters:
 // @request.body.username, and the
-// @request.body.password so we can authenticate if a user even wants to delete their account
+// @request.body.password, so we can authenticate if a user even wants to delete their account
 router.delete("/", auth.authenticateToken, async (req, res) => {
-    // Var to determine is a user is ready to delete.
-    let readyToDelete = false;
-
     // This is the user object that was sent with the request body.
-    const user = {
-        username: req.body.username,
-        password: req.body.password,
-    };
+    let user = new deleteUser(req.query.username, req.body.password);
 
     // Here we are checking if the username or password was sent empty, if so: return an error
     for (let item in user) {
@@ -39,48 +40,36 @@ router.delete("/", auth.authenticateToken, async (req, res) => {
         // If error, then we send back the error
         if (err) {
             res.status(500).send({detail: err.stack});
+            return;
         }
         // If the server returns nothing, then we know the user does not exist
         else if (sqlRes.rowCount === 0) {
             res.status(400).send({detail: "User does not exist"});
+            return;
         }
-        // else we know the user exists, then we start to authenticate them
-        else {
-            try {
-                // Here we are comparing the hashed passwords to check if they match, if they do, then we know we are ready to delete the user.
-                if (await bcrypt.compare(user.password, sqlRes.rows[0].password)) {
-                    readyToDelete = true;
-                }
-                // else we know the password they entered was incorrect
-                else {
-                    res.status(400).send({detail: "Password Incorrect."});
-                }
-            } catch (ex) {
-                res.status(500).send({detail: ex});
-            }
 
-            // Then we check if readyToDelete == true
-            if (!readyToDelete) {
-                res.status(400).send({detail: "Not ready to delete user."})
+        // Here we are comparing the hashed passwords to check if they match, if they do, then we know we are ready to delete the user.
+        if (!await bcrypt.compare(user.password, sqlRes.rows[0].password)) {
+            res.status(400).send({detail: "Password Incorrect."});
+            return;
+        }
+
+        // These are the deletion queries
+        const deleteQuery = "DELETE FROM users WHERE username=$1";
+        const deleteValues = [user.username];
+
+        // Here we are querying the database to delete the user, if error: then we just send back an error
+        pool.query(deleteQuery, deleteValues, (errDelete, sqlResDelete) => {
+            if (errDelete) {
+                res.status(500).send({detail: errDelete.stack});
                 return;
             }
 
-            // These are the deletion queries
-            const deleteQuery = "DELETE FROM users WHERE username=$1";
-            const deleteValues = [user.username];
-
-            // Here we are querying the database to delete the user, if error: then we just send back an error
-            pool.query(deleteQuery, deleteValues, (errDelete, sqlResDelete) => {
-                if (errDelete) {
-                    res.status(500).send({detail: errDelete.stack});
-                } else {
-                    // If no error, then we just send back a successfull message
-                    res.status(201).send({
-                        detail: `'${user.username}' has been succesfully deleted`,
-                    });
-                }
+            // If no error, then we just send back a successful message
+            res.status(201).send({
+                detail: `'${user.username}' has been successfully deleted`,
             });
-        }
+        });
     });
 });
 
