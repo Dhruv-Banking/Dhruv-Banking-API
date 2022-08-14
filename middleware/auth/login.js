426852983979f0dfc4e76772d7893f335ae422c2
Dhruv-Rayat1 <dhruvrayat50@gmail.com>
2022-08-15 11:45:55 +1200

    Refactoring login.js

--- middleware/auth/login.js
+++ middleware/auth/login.js
@@ -2,9 +2,9 @@
 const express = require("express"); // Express API
 const bcrypt = require("bcrypt"); // Encryption
 let router = express.Router(); // Router
-const { Pool } = require("pg"); // Querying the database
+const {Pool} = require("pg"); // Querying the database
 const jwt = require("jsonwebtoken"); // Json Web Token
-require("dotenv").config({ path: "../../.env" }); // Dotenv
+require("dotenv").config({path: "../../.env"}); // Dotenv
 const roles = require("../roles/roleData");
 
 // Connection string from the dotenv file
@@ -12,67 +12,71 @@ const connectionString = process.env.CONNECTIONSTRING;
 
 // Creating a new connection pool
 const pool = new Pool({
-  connectionString,
+    connectionString,
 });
+
+// User class
+class UserToken {
+    constructor(name, role, password) {
+        this.name = name;
+        this.role = role;
+        this.password = password;
+    }
+
+    get returnObject() {
+        return {
+            name: this.name,
+            role: this.role,
+            password: this.password,
+        }
+    }
+}
+
 
 // This is the post endpoint to get a token.
 // It takes 2 parameters:
 // @request.body.name
 // @request.body.password
 router.post("/", async (req, res) => {
-  // Querying the database
-  const query = "SELECT * FROM users WHERE username=$1";
-  const values = [req.body.name];
+    // Querying the database
+    const query = "SELECT * FROM users WHERE username=$1";
+    const values = [req.body.name];
 
-  try {
     // Here we are querying the database to get the use data
     pool.query(query, values, async (err, sqlRes) => {
-      if (err) res.status(500).send();
-      else if (sqlRes.rowCount === 0) {
-        // Since a user that returns nothing means they don't exist.
-        const postUser = {
-          username: req.body.name,
-          role: roles.viewer,
-          password: req.body.password,
-        };
+        if (err) res.status(500).send();
+        else if (sqlRes.rowCount === 0) {
+            // Post user class.
+            let postUser = new UserToken(req.body.name, roles.viewer, req.body.password);
+            const accessToken = generateAccessTokenViewer(postUser.returnObject);
 
-        const accessToken = generateAccessTokenViewer(postUser);
-
-        res.send({ postUserToken: accessToken });
-      }
-      // Else we know the user does exist, therefore we continue
-      else {
-        // User from the SQL data
-        const user = {
-          name: sqlRes.rows[0].username,
-          password: sqlRes.rows[0].password,
-          role: sqlRes.rows[0].role,
-        };
-
-        // If the hased password, and the password requested are the same, then we return true
-        if (await bcrypt.compare(req.body.password, user.password)) {
-          // Generate the access token
-          const accessToken = generateAccessToken(user);
-
-          // Then we send back the access token.
-          res.send({ accessToken: accessToken });
-        } else {
-          res.status(400).send({ detail: "Incorrect password" });
+            return res.send({postUserToken: accessToken});
         }
-      }
+
+        const sql = sqlRes.rows[0];
+
+        let user = new UserToken(sql.username, sql.role, sql.password);
+
+        // If the hashed password, and the password requested are the same, then we return true
+        if (!(await bcrypt.compare(req.body.password, user.password))) {
+            return res.status(400).send({detail: "Incorrect password"});
+        }
+
+        // Generate the access token
+        const accessToken = generateAccessToken(user.returnObject);
+
+        // Then we send back the access token.
+        res.send({accessToken: accessToken});
     });
-  } catch {
-    res.status(500).send();
-  }
 });
 
 // Function to generate the access token.
 function generateAccessToken(user) {
-  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1d" });
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: "1d"});
 }
 
 function generateAccessTokenViewer(user) {
-  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "15m" });
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: "15m"});
 }
 
 module.exports = router;
