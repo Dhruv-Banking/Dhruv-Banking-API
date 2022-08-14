8dfd6edac1feb53d722f7e207cf4c040dc37d2a6
Dhruv-Rayat1 <dhruvrayat50@gmail.com>
2022-08-15 11:51:21 +1200

    FORMATTINGGGGGGGGGGGG

--- middleware/flag-ip-address/flagIpAddress.js
+++ middleware/flag-ip-address/flagIpAddress.js
@@ -3,47 +3,47 @@ const roleData = require("../roles/roleData");
 const jwt = require("jsonwebtoken");
 
 function flagIpAddress(req, res, next) {
-  const ip = req.ip;
-  const query = "SELECT * FROM flagged_ips WHERE ip=$1";
-  const values = [ip];
+    const ip = req.ip;
+    const query = "SELECT * FROM flagged_ips WHERE ip=$1";
+    const values = [ip];
 
-  // check to see if the ip exists.
-  pool.query(query, values, (err, sqlres) => {
-    if (err) {
-      res.staus(500);
-      return res.send({ detail: "Internal Server Error" });
-    } else if (sqlres.rowCount === 0) {
-      // else if they don't exist, then we just add them
-      const addUserQuery = "INSERT INTO flagged_ips (ip) VALUES ($1)";
-      const addUserValues = values;
-      pool.query(addUserQuery, addUserValues, (error, sqlResTwo) => {
-        if (error) {
-          res.staus(500);
-          return res.send({ detail: "Internal Server Error" });
+    // check to see if the ip exists.
+    pool.query(query, values, (err, sqlres) => {
+        if (err) {
+            res.staus(500);
+            return res.send({detail: "Internal Server Error"});
+        } else if (sqlres.rowCount === 0) {
+            // else if they don't exist, then we just add them
+            const addUserQuery = "INSERT INTO flagged_ips (ip) VALUES ($1)";
+            const addUserValues = values;
+            pool.query(addUserQuery, addUserValues, (error, sqlResTwo) => {
+                if (error) {
+                    res.staus(500);
+                    return res.send({detail: "Internal Server Error"});
+                } else {
+                    next();
+                }
+            });
         } else {
-          next();
+            // IF they are god, then we let them make the account
+            const authHeader = req.headers["authorization"];
+            const token = authHeader && authHeader.split(" ")[1]; // Splitting because it goes: "Bearer [space] TOKEN"
+            let decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+            let role = decoded.role;
+
+            if (role === roleData.god || role === roleData.admin) {
+                next();
+            } else {
+                res.status(400);
+                return res.send({
+                    detail: "You already exist, please use your previous account",
+                });
+            }
         }
-      });
-    } else {
-      // IF they are god, then we let them make the the accunt
-      var authHeader = req.headers["authorization"];
-      const token = authHeader && authHeader.split(" ")[1]; // Spliting becase it goes: "Bearer [space] TOKEN"
-      let decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-
-      let role = decoded.role;
-
-      if (role === roleData.god || role === roleData.admin) {
-        next();
-      } else {
-        res.status(400);
-        return res.send({
-          detail: "You already exist, please use your previous account",
-        });
-      }
-    }
-  });
+    });
 }
 
 module.exports = {
-  flagIpAddress,
+    flagIpAddress,
 };
--- middleware/roles/authTokenPostUser.js
+++ middleware/roles/authTokenPostUser.js
@@ -2,31 +2,30 @@
 const jwt = require("jsonwebtoken");
 
 function returnUserName(token) {
-  let decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-  return decoded.name;
+    let decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    return decoded.name;
 }
 
 // ------------------- POST ------------------- //
 function authUserPostUsername(req, res, next) {
-  var authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1]; // Spliting becase it goes: "Bearer [space] TOKEN"
-  if (token === null) return res.sendStatus(401); // If the token sent is null, then we know there is no token to be verified
+    var authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1]; // Spliting becase it goes: "Bearer [space] TOKEN"
+    if (token === null) return res.sendStatus(401); // If the token sent is null, then we know there is no token to be verified
 
-  // Saving the returned role to the var role
-  let tokenUsername = returnUserName(token);
+    // Saving the returned role to the var role
+    let tokenUsername = returnUserName(token);
 
-  const queryUsername = req.query.username;
-  if (tokenUsername === queryUsername) {
-    next();
-  } else {
-    res.status(400);
-    return res.send({
-      detail:
-        "Please get a token with the same username as the user you are trying to update",
-    });
-  }
+    const queryUsername = req.query.username;
+    if (tokenUsername === queryUsername) {
+        next();
+    } else {
+        res.status(400);
+        return res.send({
+            detail: "Please get a token with the same username as the user you are trying to update",
+        });
+    }
 }
 
 module.exports = {
-  authUserPostUsername,
+    authUserPostUsername,
 };
