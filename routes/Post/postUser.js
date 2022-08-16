a2f360836ffaf548cdc81e6363c95e45120b7661
Dhruv-Rayat1 <dhruvrayat50@gmail.com>
2022-08-16 19:09:57 +1200

    Removed unnecessary variable

--- routes/Post/postUser.js
+++ routes/Post/postUser.js
@@ -23,9 +23,6 @@ router.post("/", auth.authenticateToken, authTokenPost.authRolePostUser, flagIP.
     // var to make the user body easier to read.
     const body = req.body;
 
-    // Variable to check if the body is correct.
-    let correctBody = true;
-
     // User object
     let user = new userClass(body.username, body.firstname, body.lastname, body.email, body.password, "BASIC", 0, 0);
 
@@ -57,28 +54,26 @@ router.post("/", auth.authenticateToken, authTokenPost.authRolePostUser, flagIP.
             return;
         }
 
-        // If there were no previous errors, then we continue
-        if (correctBody) {
-            // Hash password since we know everything is fine
-            user.password = await bcrypt.hash(user.password, 12);
+        // PUT IN FUNCTION
+        // Hash password since we know everything is fine
+        user.password = await bcrypt.hash(user.password, 12);
 
-            // Query, and values to post the user to the database
-            const query = "INSERT INTO users (uuid, username, firstname, lastname, email, password, savings, checkings, role) VALUES($1, $2, $3, $4, $5, $6, $7, $8 , $9)";
-            const values = [user.uuid, user.username, user.firstname, user.lastname, user.email, user.password, user.savings, user.checkings, user.role,];
+        // Query, and values to post the user to the database
+        const query = "INSERT INTO users (uuid, username, firstname, lastname, email, password, savings, checkings, role) VALUES($1, $2, $3, $4, $5, $6, $7, $8 , $9)";
+        const values = [user.uuid, user.username, user.firstname, user.lastname, user.email, user.password, user.savings, user.checkings, user.role,];
 
-            // Here we are just posting the user to the database
-            pool.query(query, values, (err, sqlRes) => {
-                // If error, then we send back the error
-                if (err) {
-                    res.status(500).send({detail: err.stack});
-                    return;
-                }
-                // else we know the user does exist
-                res.status(201).send({
-                    detail: `${user.username} has been succesfully created!`,
-                });
+        // Here we are just posting the user to the database
+        pool.query(query, values, (err, sqlRes) => {
+            // If error, then we send back the error
+            if (err) {
+                res.status(500).send({detail: err.stack});
+                return;
+            }
+            // else we know the user does exist
+            res.status(201).send({
+                detail: `${user.username} has been succesfully created!`,
             });
-        }
+        });
     });
 });
 
