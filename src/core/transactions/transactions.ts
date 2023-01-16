c604652639218c0be370b4c8ddc46a6ad58bbc48
Dhruv Rayat <dhruvrayat50@gmail.com>
2023-01-16 14:52:58 +1300

    Feature: Added the code to update the transactions of a use

---
+++ src/core/transactions/transactions.ts
@@ -0,0 +1,39 @@
+import internal from "stream";
+import { pool } from "../database/pool";
+
+/*
+// One account to another
+jsonToAppend = {
+    from: <checkingsToSavings || savingsToCheckings>,
+    amount: <some int>,
+    time: 
+}
+*/
+
+/*
+// One user to another
+jsonToAppend = {
+    userTo: <user sent money>,
+    amount: <somt int>,
+    time: 
+}
+*/
+
+async function updateCheckingsToSavings(
+  username: string,
+  amount: number,
+  time: object
+): Promise<void> {}
+
+async function updateSavingsToCheckings(
+  username: string,
+  amount: number,
+  time: object
+): Promise<void> {}
+
+async function updateToAnotherUser(
+  username: string,
+  amount: number,
+  userTo: string,
+  time: object
+): Promise<void> {}
