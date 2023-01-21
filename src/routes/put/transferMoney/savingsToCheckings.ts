4b449ebc4fc623fe06cccb3b4ed4d5dcd9e6f1c4
Dhruv Rayat <dhruvrayat50@gmail.com>
2023-01-21 13:36:23 +1300

    Feature: Middleware for `PUT` endpoints is done

--- src/routes/put/transferMoney/savingsToCheckings.ts
+++ src/routes/put/transferMoney/savingsToCheckings.ts
@@ -11,75 +11,80 @@ import { authToken } from "../../../core/auth/auth";
 
 const router = express.Router();
 
-router.put("/", async (req: Request, res: Response) => {
-  if (typeof req.body.amount === "string" || req.body.amount === undefined)
-    return res
-      .status(400)
-      .send({ detail: "Amount must be of type int, and not empty" });
+router.put(
+  "/",
+  authToken,
+  transferMoneyMiddleware,
+  async (req: Request, res: Response) => {
+    if (typeof req.body.amount === "string" || req.body.amount === undefined)
+      return res
+        .status(400)
+        .send({ detail: "Amount must be of type int, and not empty" });
 
-  const arrOfItems = [
-    req.body.username,
-    req.body.amount.toString(),
-    req.body.password,
-  ];
+    const arrOfItems = [
+      req.body.username,
+      req.body.amount.toString(),
+      req.body.password,
+    ];
 
-  if (req.body.amount <= 0)
-    return res
-      .status(400)
-      .send({ detail: "Can not accept money if it's less than 0" });
+    if (req.body.amount <= 0)
+      return res
+        .status(400)
+        .send({ detail: "Can not accept money if it's less than 0" });
 
-  const user: SendMoneyToMyself = {
-    username: req.body.username,
-    password: req.body.password,
-    amount: req.body.amount,
-  };
+    const user: SendMoneyToMyself = {
+      username: req.body.username,
+      password: req.body.password,
+      amount: req.body.amount,
+    };
 
-  if (!verifyArray(arrOfItems))
-    return res.status(400).send({ detail: "Provide all items" });
+    if (!verifyArray(arrOfItems))
+      return res.status(400).send({ detail: "Provide all items" });
 
-  let sqlResGetPassSavings;
+    let sqlResGetPassSavings;
 
-  const queryGetPassSavings = {
-    text: "SELECT savings, password FROM public.users WHERE username=$1",
-    values: [user.username],
-  };
+    const queryGetPassSavings = {
+      text: "SELECT savings, password FROM public.users WHERE username=$1",
+      values: [user.username],
+    };
 
-  try {
-    sqlResGetPassSavings = await pool.query(queryGetPassSavings);
-  } catch (err: any) {
-    return res.status(500).send({ detail: err.stack });
+    try {
+      sqlResGetPassSavings = await pool.query(queryGetPassSavings);
+    } catch (err: any) {
+      return res.status(500).send({ detail: err.stack });
+    }
+
+    if (sqlResGetPassSavings.rowCount === 0)
+      return res.status(400).send({ detail: "You do not exist" });
+
+    if (
+      !(await comparePassword(
+        user.password,
+        sqlResGetPassSavings.rows[0].password
+      ))
+    )
+      return res.status(400).send({ detail: "User not authenticated" });
+
+    if (user.amount > sqlResGetPassSavings.rows[0].savings)
+      return res.status(400).send({ detail: "Insufficient funds" });
+
+    const queryUpdateMoney = {
+      text: "UPDATE users SET savings=savings-$1, checkings=checkings+$2 WHERE username=$3",
+      values: [user.amount, user.amount, user.username],
+    };
+
+    try {
+      await pool.query(queryUpdateMoney);
+    } catch (err: any) {
+      return res.status(500).send({ detail: err.stack });
+    }
+
+    await updateSavingsToCheckings(user.username, user.amount, new Date());
+
+    return res.status(201).send({
+      detail: `Successfully transered ${user.amount} from savings to checkings`,
+    });
   }
-
-  if (sqlResGetPassSavings.rowCount === 0)
-    return res.status(400).send({ detail: "You do not exist" });
-
-  if (
-    !(await comparePassword(
-      user.password,
-      sqlResGetPassSavings.rows[0].password
-    ))
-  )
-    return res.status(400).send({ detail: "User not authenticated" });
-
-  if (user.amount > sqlResGetPassSavings.rows[0].savings)
-    return res.status(400).send({ detail: "Insufficient funds" });
-
-  const queryUpdateMoney = {
-    text: "UPDATE users SET savings=savings-$1, checkings=checkings+$2 WHERE username=$3",
-    values: [user.amount, user.amount, user.username],
-  };
-
-  try {
-    await pool.query(queryUpdateMoney);
-  } catch (err: any) {
-    return res.status(500).send({ detail: err.stack });
-  }
-
-  await updateSavingsToCheckings(user.username, user.amount, new Date());
-
-  return res.status(201).send({
-    detail: `Successfully transered ${user.amount} from savings to checkings`,
-  });
-});
+);
 
 module.exports = router;
