81fc1d3a850a429f0cdd508254488e196d068038
Dhruv Rayat <dhruvrayat50@gmail.com>
2023-01-22 23:26:55 +1300

    Update: Gotta add the spicy comment :hot_pepper::hot_pepper::hot_pepper:

--- src/routes/dhruv/createTables.ts
+++ src/routes/dhruv/createTables.ts
@@ -1,3 +1,4 @@
+// This file is in case I hit the database with a good ol' `sudo rm -rf /` or forget the ssh keys :skull:
 import express, { Request, Response } from "express";
 
 import { authToken } from "../../core/auth/auth";
@@ -8,8 +9,8 @@ const router = express.Router();
 
 router.post(
   "/",
-  allDhruvEndpointMiddleware,
   authToken,
+  allDhruvEndpointMiddleware,
   async (req: Request, res: Response) => {
     let result = await createTables();
 
