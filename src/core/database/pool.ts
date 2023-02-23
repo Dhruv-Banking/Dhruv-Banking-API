607a2881ac957e20ae6427aca86d0efc6f9ddc7e
Dhruv Rayat <dhruvrayat50@gmail.com>
2023-02-23 23:34:04 +1300

    Update: Change to make it work with linux

--- src/core/database/pool.ts
+++ src/core/database/pool.ts
@@ -3,7 +3,7 @@ import { PoolClient } from "pg";
 require("dotenv").config({ path: `${__dirname}/../../.env` });
 
 // Connection string
-const user = process.env.USER;
+const user = process.env.USER_PG;
 const host = process.env.HOST;
 const database = process.env.DATABASE;
 const password = process.env.PASSWORD;
